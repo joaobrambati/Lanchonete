35e168bdf6158bd1719a7f1f0f55aea9be6d3f88
João Vitor <joaobrambati@gmail.com>
2024-08-11 18:49:57 -0300

    Modelo de Domínio

---
+++ Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace Lanchonete.Models {
+    public class Categoria {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; } 
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ Models/Lanche.cs
@@ -0,0 +1,16 @@
+﻿namespace Lanchonete.Models {
+    public class Lanche {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public double Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool LanchePreferido { get; set; }
+        public bool TemEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
