f01b072d3f0a15dc7b7c70a933ed61cb4f29e40d
João Vitor <joaobrambati@gmail.com>
2024-08-11 15:51:43 -0300

    Classe Startup e teste Demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,17 +4,11 @@ using System.Diagnostics;
 
 namespace Lanchonete.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController (ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
         public IActionResult Index () {
             return View();
         }
 
-        public IActionResult Privacy () {
+        public IActionResult Demo () {
             return View();
         }
 
