a65684740bddb03fcf3a6908abb58751cac3941a
João Brambati <joao.brambati@litoraltextilcom.br>
2024-08-12 12:34:03 -0300

    Implementação Padrão Repository Categoria

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using Lanchonete.Context;
+using Lanchonete.Models;
+using Lanchonete.Repositories.Interfaces;
+
+namespace Lanchonete.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lanchonete.Models;
+
+namespace Lanchonete.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
