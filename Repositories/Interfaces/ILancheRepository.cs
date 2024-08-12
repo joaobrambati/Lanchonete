20a6f565970934689003b5628f9027030b58abff
João Brambati <joao.brambati@litoraltextilcom.br>
2024-08-12 13:00:53 -0300

    Implementação Padrão Repository Lanche

---
+++ Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lanchonete.Models;
+
+namespace Lanchonete.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheByID(int lancheId);
+    }
+}
---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,29 @@
+﻿using Lanchonete.Context;
+using Lanchonete.Models;
+using Lanchonete.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanchonete.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(l => l.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                   .Where(l => l.LanchePreferido)
+                                   .Include(l => l.Categoria);
+
+        public Lanche GetLancheByID(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+        }
+
+    }
+}
