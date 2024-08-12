f771aaa857ef0080246a405695d0a7a9359e0004
João Brambati <joao.brambati@litoraltextilcom.br>
2024-08-12 13:44:06 -0300

    Controller e View para exibir os lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using Lanchonete.Context;
+using Lanchonete.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanchonete.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+
+    }
+}
