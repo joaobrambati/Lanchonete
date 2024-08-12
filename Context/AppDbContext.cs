5a2afd46ecf71a3b1dedc67dc03d44cd8936f980
João Brambati <joao.brambati@litoraltextilcom.br>
2024-08-12 09:50:32 -0300

    Data Annotations

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lanchonete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanchonete.Context {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Lanchonete.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
