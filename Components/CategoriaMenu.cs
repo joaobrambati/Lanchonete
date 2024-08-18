9f75791b05452d1ccd44d84ca427802662e0af3e
João Vitor <joaobrambati@gmail.com>
2024-08-18 16:31:05 -0300

    ViewComponent exibir menu com as Categorias

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,23 @@
+﻿using Lanchonete.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanchonete.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu (ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias
+                            .OrderBy(x => x.CategoriaNome);
+
+            return View(categorias);
+        }
+    }
+}
