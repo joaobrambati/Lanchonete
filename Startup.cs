e8408fba7f8439747531ca1f68fbc5d4cb4eb145
João Vitor <joaobrambati@gmail.com>
2024-08-18 14:35:45 -0300

    Rota Exibir Lanches por Categoria

--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,12 @@ namespace Lanchonete;
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => {
+
+                endpoints.MapControllerRoute(
+                    name: "categoriaFiltro",
+                    pattern: "Lanche/{action}/{categoria?}",
+                    defaults: new { Controller = "Lanche", action = "List"});
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
