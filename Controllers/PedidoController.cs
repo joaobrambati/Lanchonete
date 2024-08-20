ec69632bd1ed275c5f16092fafe4e428df311e87
João Brambati <joao.brambati@litoraltextilcom.br>
2024-08-20 12:52:15 -0300

    Criando PedidoController e a View Checkout

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using Lanchonete.Models;
+using Lanchonete.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanchonete.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet] // Formulário de confirmação
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost] // Processamento
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View(pedido);
+        }
+
+    }
+}
